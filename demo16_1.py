ae768bf3ad8c40a50f5531b1f5b15e7456c6ff43
cat <15031937570@163.com>
2018-03-03 12:38:02 +0800

    $date

---
+++ demo16_1.py
@@ -0,0 +1,9 @@
+#-*- coding:utf-8 -*-
+import sys
+sys.path.append('/workspace')
+import python.demo16 as tc
+
+print("32摄氏度= %.2f华氏度" % tc.c2f(32))
+print("99华氏度= %.2f摄氏度" % tc.f2c(99))
+print(__name__)
+print(tc.__name__)
